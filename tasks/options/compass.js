b0e53d41b665d82a7726b2a757082416142fe071
sprockow <sprockow@gmail.com>
2014-02-10 19:14:59 -0800

    adding support for source map in compass

--- tasks/options/compass.js
+++ tasks/options/compass.js
@@ -1,17 +1,18 @@
 module.exports = {
-  options: {
-    sassDir: "app/styles",
-    cssDir: "tmp/result/assets",
-    generatedImagesDir: "tmp/result/assets/images/generated",
-    imagesDir: "public/assets/images",
-    javascriptsDir: "app",
-    fontsDir: "public/assets/fonts",
-    importPath: ["vendor"],
-    httpImagesPath: "/assets/images",
-    httpGeneratedImagesPath: "/assets/images/generated",
-    httpFontsPath: "/assets/fonts",
-    relativeAssets: false,
-    debugInfo: true
-  },
-  compile: {}
+    options: {
+        sassDir: "app/styles",
+        cssDir: "tmp/result/assets",
+        generatedImagesDir: "tmp/result/assets/images/generated",
+        imagesDir: "public/assets/images",
+        javascriptsDir: "app",
+        fontsDir: "public/assets/fonts",
+        importPath: ["vendor"],
+        httpImagesPath: "/assets/images",
+        httpGeneratedImagesPath: "/assets/images/generated",
+        httpFontsPath: "/assets/fonts",
+        relativeAssets: false,
+        debugInfo: true,
+        sourcemap: true
+    },
+    compile: {}
 };
